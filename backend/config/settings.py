879bf4d5d5f9ba351057c6b88b218a50cd4e9103
Facundo <facundoserradev@gmail.com>
2025-10-28 19:58:38 -0300

    Actualiza configuraciones de ALLOWED_HOSTS y versiones de dependencias en requirements.txt

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,exp://10.10.20.143:8081').split(',')
 # En desarrollo permite cualquier host para facilitar pruebas desde LAN/Expo Go
 if DEBUG:
     ALLOWED_HOSTS = ['*']
